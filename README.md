ea22abe2328dc92069497d78649eb8ed3bd6e5c6
Adam Fanello <kernwig@gmail.com>
2020-02-15 17:58:47 -0800

    Init version

---
+++ README.md
@@ -0,0 +1,18 @@
+SquareCue
+=========
+by Adam Fanello
+
+Background
+----------
+
+I was making cue sheets in a word processor. Each song had a document.
+In each document was a whole cue sheet for each level of dance I'd made choreography for.
+I liked to highlight the actual cues. Then I wanted to add little beat markers to help me keep time.
+Adding these meant editing every document, every level, nearly every line - and fighting the word processor
+that always handled the font transitions exactly opposite of what I wanted...
+
+    Argh!!!  
+
+That's it. I switched to markup. I developed SquareCue so that I could eliminate duplication, stop fighting
+with a word processor, and change my styles once and have _everything_ update automatically!
+
---
+++ code/squarecue.js
@@ -0,0 +1,29 @@
+var select = '<select id="dancelevel" onchange="document.body.className = this.value">';
+var levels = [
+    {value: 'abc', display: 'ABC Core'},
+    {value: 'abca', display: 'ABC A-Dance'},
+    {value: 'abcb', display: 'ABC B-Dance'},
+    {value: 'abcc', display: 'ABC C-Dance'},
+    {value: 'basic', display: 'Basic'},
+    {value: 'ms', display: 'Mainstream'},
+    {value: 'plus', display: 'Plus'},
+    {value: 'a1', display: 'A1'},
+    {value: 'a2', display: 'A2/Advanced'},
+];
+var firstLevel = null;
+for (var idx=0; idx < levels.length; ++idx) {
+    var level = levels[idx];
+    var levelElements = document.getElementsByTagName(level.value);
+    if (levelElements.length) {
+        select += '<option value="' + level.value + '">' + level.display + '</option>';
+        firstLevel = firstLevel || level.value;
+    }
+}
+select += '</select>';
+
+if (firstLevel) {
+    var h1 = document.getElementsByTagName('h1')[0];
+    h1.insertAdjacentHTML('afterend', select);
+    document.getElementById('dancelevel').selectedIndex = 0;
+    document.body.className = firstLevel;
+}
